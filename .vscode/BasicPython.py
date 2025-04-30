7a763de0eac18b3c07f8d62adacc992b33f226cf
AnkitaAKharpade <kharpadeankita9@gmail.com>
2025-04-30 17:02:21 +0530

    Update BasicPython.py

--- .vscode/BasicPython.py
+++ .vscode/BasicPython.py
@@ -497,6 +497,15 @@ for i in range(10,0-1):
     for j in range(i,0-1):
         print("*",end='  ')
 
+for i in range(5):
+    for j in range(i+1):
+        print("*",end=' ')
+        print()
+for i in range(5,0,-1):
+    for j in range(5,i,-1):
+        print("*",end=' ')
+        print()
+
 
 
 
