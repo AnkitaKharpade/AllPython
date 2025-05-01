eaef6af76e7926e486a7144c825d77abd3833494
AnkitaAKharpade <kharpadeankita9@gmail.com>
2025-05-01 11:41:24 +0530

    Update BasicPython.py

--- .vscode/BasicPython.py
+++ .vscode/BasicPython.py
@@ -506,6 +506,12 @@ for i in range(5,0,-1):
         print("*",end=' ')
         print()
 
+# print table using loops
+for i in range(1,6):
+    for j in range(1,6):
+        print("i*j",end='  ')
+        print()
+
 
 
 
